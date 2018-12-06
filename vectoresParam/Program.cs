f55018d90ebc013c348d0e9f74569315cd20097b
Estudiante PUCESE <estudiante@pucese.edu.ec>
2018-12-06 09:47:11 -0500

    el correcto

--- vectoresParam/Program.cs
+++ vectoresParam/Program.cs
@@ -14,22 +14,44 @@ namespace vectoresParam
             int[] x = new int[MAX];
             leer(x);
             imprimir(x);
+            if (buscar(x,15)==true)
+            Console.WriteLine("\n15 encontrado");
+            else 
+            Console.WriteLine("\n15no encontrado");
             Console.ReadKey();
         }
         public static void leer(int[] x)
         {
-            for(int i=0; i>=MAX; i++)
+            for (int i = 0; i < MAX; i++)
             {
                 Console.WriteLine("\nIngresa el elemento{0}", i + 1);
                 x[i] = Int32.Parse(Console.ReadLine());
             }
         }
-        public static void imprimir(int[]x)
+        public static void imprimir(int[] x)
         {
-            foreach(int num in x)
+            foreach (int num in x)
             {
                 Console.WriteLine("\nElemento{0}", num);
             }
+            
         }
+        public static bool buscar(int[]x,int y)
+        {
+            bool encontrado = false;
+            foreach(int num in x)
+            {
+                if (num == y)
+                {
+                    encontrado = true;
+                    break;
+                   
+                }
+            }
+            return encontrado;
+        }
+
+
+
     }
 }
