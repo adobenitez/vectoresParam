8cb20a17df7598a9b331382bc23edd7fb5511bb0
Adonis Benitez <adonis.b3.lil_@hotmail.com>
2018-12-16 13:00:25 -0500

    Vector de nombres de personas, luego ordenarlo e imprimirlo.

--- vectoresParam/Program.cs
+++ vectoresParam/Program.cs
@@ -8,60 +8,65 @@ namespace vectoresParam
 {
     class Program
     {
+       
+       
         const int MAX = 10;
         static void Main(string[] args)
         {
             int[] x = new int[MAX];
-            string[]nombres=new sring[MAX]
+            string[] nombres = new string[MAX];
+            //************//
             leer(x);
             imprimir(x);
-            if (buscar(x, 15) == true)
-                Console.WriteLine("\n15 encontrado");
-            else
-                Console.WriteLine("\n15no encontrado");
-
-            if (buscar2(x, 20) == true)
-                Console.WriteLine("\n20 encontrado");
-            else
-                Console.WriteLine("\n20 no encontrado");
-            Console.ReadKey();
             ordenar(x);
-            Console.WriteLine("\nVector ordenado");
+            Console.WriteLine("Vector Ordenar");
             imprimir(x);
+
+            if (buscar(x, 15))
+                Console.WriteLine("\n15 ENCONTRADO");
+            else
+                Console.WriteLine("\n15 NO ENCONTRADO");
+
+            if (buscar2(x, 25))
+                Console.WriteLine("\n25 ENCONTRADO");
+            else
+                Console.WriteLine("\n25 NO ENCONTRADO");
+
+
+            //TRABAJADDO CON EL VECTOR DE CADENA DE CARACTERES//
+            Console.WriteLine("\nIngrese los nombres");
+            leerNombres(nombres);
+            imprimirNombres(nombres);
+
+            ordenarNombres(nombres);
+            Console.WriteLine("Vector Ordenar");
+            imprimirNombres(nombres);
+            Console.ReadKey();
+
+
+
+
         }
         public static void leer(int[] x)
         {
             for (int i = 0; i < MAX; i++)
             {
-                Console.WriteLine("\nIngresa el elemento{0}", i + 1);
+                Console.WriteLine("\nIngrese el elemento{0}", i + 1);
                 x[i] = Int32.Parse(Console.ReadLine());
+
             }
         }
-        public static void leernombres(string [] x)
-        {
-            for (int i = 0; i < MAX; i++)
-            {
-                Console.WriteLine("\nIngresa el elemento{0}", i + 1);
-                x[i] = (Console.ReadLine());
-            }
-        }
+
         public static void imprimir(int[] x)
         {
             foreach (int num in x)
             {
-                Console.WriteLine("\nElemento{0}", num);
+                Console.WriteLine("\nElemento {0}", num);
             }
-
         }
-        public static void imprimirnombres(int[] x)
-        {
-            foreach (string num in x)
-            {
-                Console.WriteLine("\nElemento{0}", num);
-            }
 
-        }
         public static bool buscar(int[] x, int y)
+
         {
             bool encontrado = false;
             foreach (int num in x)
@@ -70,11 +75,14 @@ namespace vectoresParam
                 {
                     encontrado = true;
                     break;
-
                 }
             }
             return encontrado;
+
+
+
         }
+
         public static bool buscar2(int[] x, int y)
         {
             bool encontrado = false;
@@ -87,19 +95,71 @@ namespace vectoresParam
             }
             return encontrado;
         }
-        public void ordenar(int[] x)
+
+        public static void ordenar(int[] x)
         {
             int aux = 0;
-            for(int i=0;i<x.Length;i++)
-             for(int j=i+1;j<x.Length;j++)
-             
+            for (int i = 0; i < x.Length; i++)
+                for (int j = i + 1; j < (x.Length); j++)
+
                     if (x[i] > x[j])
                     {
                         aux = x[i];
                         x[i] = x[j];
                         x[j] = aux;
-                    }          
+                    }
         }
-   
+
+
+
+
+        public static void leerNombres(string[] x)
+        {
+            for (int i = 0; i < MAX; i++)
+            {
+                Console.WriteLine("\nIngrese el elemento{0}", i + 1);
+                x[i] = Console.ReadLine();
+
+            }
+        }
+        public static void OrdenarNombres(string[] x)
+        {
+            string aux = "";
+            for (int i = 0; i < x.Length; i++)
+                for (int j = i + 1; j < x.Length; j++)
+                    if (String.Compare(x[i], x[j], StringComparison.Ordinal) > 0)
+                    { //ingresar el elemento del vector
+                        aux = x[i];
+                        x[i] = x[j];
+                        x[j] = aux;
+                    }
+
+
+        }
+        public static void ordenarNombres(string[] x)
+        {
+            string aux = "";
+            for (int i = 0; i < x.Length; i++)
+            {
+                for (int j = i; j < x.Length; j++)
+                {
+                    if (String.Compare(x[i], x[j], StringComparison.Ordinal) > 0)
+                    {
+                        aux = x[i];
+                        x[i] = x[j];
+                        x[j] = aux;
+                    }
+                }
+            }
+        }
+
+        public static void imprimirNombres(string[] x)
+        {
+            foreach (string num in x)
+            {
+                Console.WriteLine("\nNombre: {0}", num);
+            }
+        }
+
     }
 }
